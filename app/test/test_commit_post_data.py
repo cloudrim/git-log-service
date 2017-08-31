f978bf79732bea2144ce0f869531e3566d4d8bce
alexzhuke <alexee_zhu@163.com>
2017-08-31 19:46:41 +0800

    add add commit api post method

--- app/test/test_commit_post_data.py
+++ app/test/test_commit_post_data.py
@@ -1,10 +1,11 @@
 import requests
 from datetime import datetime
+import json
 
 location = ""
 dt=datetime.now()
-body = {'repo_id': 2,
-        'revision': 'nnkjhohlnhkjhkgvjfhgj',
+body = json.dumps({
+    'revision': 'nnkjhohlnhkjhkgv1jf232555323hgj',
         'last_update': dt.strftime('%Y%m%d%H%M%S'),
         'author': "alexander",
         'author_email': "alex@123.com",
@@ -13,7 +14,7 @@ body = {'repo_id': 2,
         'committer_email': "alexee@234.com",
         'title': "hello",
         'message': "hello world"
-        }
+})
 
 r = requests.post('http://127.0.0.1:5000/repo/2/commit', data=body)
 print(r.text)
